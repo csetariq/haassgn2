bf5a8f992760d000a87e88eb5b9ddaab1a47c282
Tariq Kamardeen <Tariq_Kamardeen@symantec.com>
2018-11-06 15:10:28 +0530

    Create data directory if not exists

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
  */
 
 //  Dependencies
+var helpers = require('./lib/helpers');
 var server = require('./lib/server');
 
 // Container
 var app = {};
 
 app.start = function() {
+    helpers.createDirectories();
     server.start();
 };
 
